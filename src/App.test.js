a168420e97cf0a604c928555e0828aad33260dd5
sanjusandyaj <44117510+sanjusandyaj@users.noreply.github.com>
2025-11-24 13:33:12 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
 test('renders AI topic', () => {
   render(<App />);
   const text = screen.getByText(/Artificial Intelligence/i);
