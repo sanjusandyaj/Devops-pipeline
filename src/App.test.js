bd4b7db141ad4c55b65142ff999ba408b773a848
sanjusandyaj <44117510+sanjusandyaj@users.noreply.github.com>
2025-11-24 12:34:47 +0530

    App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
 test('renders AI topic', () => {
   render(<App />);
   expect(screen.getByText(/Artificial Intelligence/i)).toBeInTheDocument();
