6a336645c128de192cf9123c39cea4411698c6d0
sanjusandyaj <44117510+sanjusandyaj@users.noreply.github.com>
2025-11-24 12:07:43 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-test('renders hello world', () => {
+test('renders something', async () => {
   render(<App />);
-  const element = screen.getByText(/hello world/i);
+
+  const element = await screen.findByText(/learn react/i);
   expect(element).toBeInTheDocument();
 });
