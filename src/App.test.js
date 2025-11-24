b7a5ce9666ba29ffa6a9a28dfb755571f0896797
sanjusandyaj <44117510+sanjusandyaj@users.noreply.github.com>
2025-11-24 13:48:19 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-test('AI topic', () => {
+
+test('renders AI topic', () => {
   render(<App />);
   const text = screen.getByText(/Artificial Intelligence/i);
   expect(text).toBeInTheDocument();
