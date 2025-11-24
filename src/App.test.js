355ded64d079f90d682d99f6e7defe5e4a7017f0
sanjusandyaj <44117510+sanjusandyaj@users.noreply.github.com>
2025-11-24 12:10:55 +0530

    App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
+test('renders something', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const element = await screen.findByText(/learn react/i);
+  expect(element).toBeInTheDocument();
 });
